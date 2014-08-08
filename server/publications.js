b53d5dceab5b614244c54e10009d87e5c32e06f1
petermikitsh <peter.mikitsh@gmail.com>
2014-08-08 14:39:03 -0400

    added pub/sub for individual posts

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
+
 Posts.allow({
   insert: function(userId, doc) {
     // only allow posting if you are logged in
