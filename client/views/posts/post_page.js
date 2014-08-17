8606c3aee65929ef42ea7b040b3fed3236ceb40c
petermikitsh <peter.mikitsh@gmail.com>
2014-08-17 10:46:46 -0400

    fixed malformed share url bug when using iron-router

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,18 +1,14 @@
 var social_links = {
-  'facebook': 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href,
-  'twitter': 'https://twitter.com/intent/tweet?url=' + window.location.href,
-  'reddit': 'http://www.reddit.com/submit?url=' + window.location.href,
-  'plus': 'https://plus.google.com/share?url=' + window.location.href,
-  'linkedin': 'https://www.linkedin.com/cws/share?url=' + window.location.href
-}
-
-var open = function open(url) {
-  return window.open(url);
-}
+  'facebook': 'https://www.facebook.com/sharer/sharer.php?u=',
+  'twitter': 'https://twitter.com/intent/tweet?url=',
+  'reddit': 'http://www.reddit.com/submit?url=',
+  'plus': 'https://plus.google.com/share?url=',
+  'linkedin': 'https://www.linkedin.com/cws/share?url='
+};
 
 Template.postPage.events({
   'click *[social]': function (e) {
-    var url = social_links[$(e.currentTarget).attr("social")];
-    open(url);
+    var url = social_links[$(e.currentTarget).attr("social")] + this.url;
+    window.open(url);
   }
 });
