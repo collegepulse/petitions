ef081f9f01c2858bcab2267431874a6c13fe9161
petermikitsh <peter.mikitsh@gmail.com>
2014-08-26 18:09:02 -0400

    updated fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,7 +12,7 @@ if (Posts.find().count() === 0) {
   var pete = Meteor.users.findOne(peteId);
   Roles.addUsersToRoles(peteId, ['admin']);
 
-  // Post with 7-day "history"
+  // Post with 7-day history
 
   var postId_seven_day = Posts.insert({
     userId: pete._id,
@@ -21,7 +21,7 @@ if (Posts.find().count() === 0) {
     title: "Build more affordable, on-campus housing.",
     description: "As there is only enough housing for half the student population, it creates a challenge to find affordably priced off-campus housing.",
     upvoters: [pete._id],
-    votes: 7
+    votes: 50
   });
 
   Scores.insert({
@@ -34,43 +34,43 @@ if (Posts.find().count() === 0) {
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(6, 'days').valueOf(),
-    score: 1,
-    votes: 1
+    score: 10,
+    votes: 10
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(5, 'days').valueOf(),
-    score: 1,
-    votes: 1
+    score: 22,
+    votes: 22
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(4, 'days').valueOf(),
-    score: 2,
-    votes: 2
+    score: 30,
+    votes: 30
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(3, 'days').valueOf(),
-    score: 4,
-    votes: 4
+    score: 44,
+    votes: 44
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(2, 'days').valueOf(),
-    score: 5,
-    votes: 5
+    score: 47,
+    votes: 47
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(1, 'day').valueOf(),
-    score: 7,
-    votes: 7
+    score: 50,
+    votes: 50
   });
 
   // Post with 3-day history
@@ -87,34 +87,6 @@ if (Posts.find().count() === 0) {
 
   Scores.insert({
     postId: postId_three_day,
-    created_at: moment().subtract(7, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_three_day,
-    created_at: moment().subtract(6, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_three_day,
-    created_at: moment().subtract(5, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_three_day,
-    created_at: moment().subtract(4, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_three_day,
     created_at: moment().subtract(3, 'days').valueOf(),
     score: 1,
     votes: 1
@@ -134,7 +106,7 @@ if (Posts.find().count() === 0) {
     votes: 4
   });
 
-
+  // Post with 0-day history
 
   var postId_no_history = Posts.insert({
     userId: pete._id,
@@ -143,55 +115,6 @@ if (Posts.find().count() === 0) {
     title: "Offer more options for students with unique dietary needs.",
     description: "Increase the number of options for vegan students at on-campus Dining Service locations.",
     upvoters: [pete._id],
-    votes: 1
-  });
-
-   Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(7, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-  
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(6, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(5, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(4, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(3, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(2, 'days').valueOf(),
-    score: 1,
-    votes: 1
-  });
-
-  Scores.insert({
-    postId: postId_no_history,
-    created_at: moment().subtract(1, 'day').valueOf(),
-    score: 1,
     votes: 1
   });
 
