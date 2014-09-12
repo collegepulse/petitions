accf6e3a19bab7f8658104be658e50e99f6eeec4
petermikitsh <peter.mikitsh@gmail.com>
2014-09-12 17:11:59 -0400

    remove name change functionality; profile ui improvements

--- collections/users.js
+++ collections/users.js
@@ -1,31 +1,4 @@
 Meteor.methods({
-  editName: function(userAttributes) {
-
-    var user = Meteor.user();
-    
-    // ensure the user is logged in
-    if (!user)
-      throw new Meteor.Error(401, "You need to login to edit name preferences.");
-
-    if (user.profile.displayName.toLowerCase() != userAttributes.profile.displayName.toLowerCase())
-      throw new Meteor.Error(422, "Full name must use same spelling.");
-
-    if (user.profile.givenName.toLowerCase() != userAttributes.profile.givenName.toLowerCase())
-      throw new Meteor.Error(422, "First name must use same spelling.");
-
-    if (user.profile.sn.toLowerCase() != userAttributes.profile.sn.toLowerCase())
-      throw new Meteor.Error(422, "Last name must use same spelling.");
-
-    Meteor.users.update({
-      _id: user._id
-    }, {
-      $set: {
-        'profile.displayName': userAttributes.profile.displayName,
-        'profile.givenName': userAttributes.profile.givenName,
-        'profile.sn': userAttributes.profile.sn
-      }
-    });
-  },
   editUserRole: function(username, role, actionType) {
 
     var loggedInUser = Meteor.user();
