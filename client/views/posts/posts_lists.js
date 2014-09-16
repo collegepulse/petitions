a1ddc81779ed0cf5c1e97c4d2e2a22f8c43c3e05
petermikitsh <peter.mikitsh@gmail.com>
2014-09-15 21:56:12 -0400

    load more button on mobile

--- client/views/posts/posts_lists.js
+++ client/views/posts/posts_lists.js
@@ -1,5 +1,8 @@
 Template.petitionGroup.events({
   'change #petition-order': function(evt) {
      Session.set("postOrder", evt.currentTarget.value);
+  },
+  'click #load-more': function () {
+    Session.set('postsLimit', Session.get('postsLimit') + 12);
   }
 });
