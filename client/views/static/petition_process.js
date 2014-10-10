34e915ff98370bd12fec7f375c725efcdfce8edc
Peter Mikitsh <peter.mikitsh@gmail.com>
2014-10-10 12:10:14 -0400

    fix date display issue on petition process page
    a missed change from prior refactoring work

--- client/views/static/petition_process.js
+++ client/views/static/petition_process.js
@@ -1,5 +1,5 @@
 Template.petitionProcess.helpers({
   threshold_last_updated: function thresholdLastUpdatedAt () {
-    return new moment(this.singleton.threshold_updated_at).format("L");
+    return new moment(Singleton.findOne().threshold_updated_at).format("L");
   }
 });
