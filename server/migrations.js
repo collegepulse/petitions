480936f524f0a248f42cf6c1a8071757c8b5a239
petermikitsh <peter.mikitsh@gmail.com>
2014-12-03 15:00:49 -0500

    subscribe users to status update emails

--- server/migrations.js
+++ server/migrations.js
@@ -1,6 +1,7 @@
 Migrations = new Meteor.Collection('migrations');
 
 Meteor.startup(function () {
+  // change default schema to include notification preferences for users
   if (!Migrations.findOne({name: "addDefaultNotificationPreferences"})) {
     Meteor.users.find().forEach(function (user) {
       Meteor.users.update(user._id, {$set: {notify: {updates: false, response: true}}});
@@ -8,6 +9,7 @@ Meteor.startup(function () {
     Migrations.insert({name: "addDefaultNotificationPreferences"});
   }
   // only necessary if upgrading from <=v1.1.1 to a newer release
+  // ensures that all required user profile attributes are defined
   if (!Migrations.findOne({name: "ensureProfilePropertyExistsForUsers"})) {
     Meteor.users.find().forEach(function (user) {
       if (!user.profile) {
@@ -21,5 +23,10 @@ Meteor.startup(function () {
       }  
     });
     Migrations.insert({name: "ensureProfilePropertyExistsForUsers"});
+  }
+  // auto-subscribe users to status update e-mails *if* they have official responses enabled
+  if (!Migrations.findOne({name: "enableStatusUpdateEmailsIfOfficialResponsesEnabled"})) {
+    Meteor.users.update({'notify.response': true}, {$set: {'notify.updates': true}});
+    Migrations.insert({name: "enableStatusUpdateEmailsIfOfficialResponsesEnabled"});
   }
 });
