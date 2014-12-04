7d61724cbfa163f78c5ef20b183a2b5c24fb548f
Peter Mikitsh <peter.mikitsh@gmail.com>
2014-12-04 13:06:31 -0500

    bump version number to v1.2.2.3

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,4 @@
 Meteor.startup(function () {
-  Singleton.update({}, {$set: { version: "v1.2.2.2" }});
+  Singleton.update({}, {$set: { version: "v1.2.2.3" }});
   process.env.MAIL_URL = Meteor.settings.MAIL_URL;
 });
