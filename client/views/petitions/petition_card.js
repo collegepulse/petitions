fa67b2a3223891f44f9a07a3b119cfe2f42d32c0
Nathan Castle <nathan.castle@outlook.com>
2015-11-15 21:40:56 -0500

    Fixed so that all responded petitions appear orange.

--- client/views/petitions/petition_card.js
+++ client/views/petitions/petition_card.js
@@ -6,12 +6,18 @@ Template.petitionCard.events({
 
 Template.petitionCard.helpers({
   isExpired: function(e) {
-    console.log(e);
     var petition = Petitions.findOne(e.petition._id);
     if(moment(petition.submitted).isBefore(moment().subtract(1, 'month'))) {
       return true;
     }
     else {
+      return false;
+    }
+  },
+  isResponded: function(e) {
+    if (e.status == "responded"){
+      return true;
+    }else{
       return false;
     }
   }
