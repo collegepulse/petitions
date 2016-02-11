e064e467c80ffe2cbd7d02114d2ae21da00828d9
Dave Lafferty <claffer2@naz.edu>
2016-02-11 16:20:32 -0500

    Email templates are now compiled at server start, to catch more errors outside of runtime.  I also use the new string.format methods I included.

--- server/mailer.js
+++ server/mailer.js
@@ -2,9 +2,14 @@
  * Uses the settings file to create default emails.  Thus if we don't have at least the MAIL 
  * key set up this module is unusable and the site won't really work.
  */
-
 if(!Meteor.settings.MAIL)
     throw new Error("You must have a Meteor.settings.MAIL attribute, please refer to the settings.json.sample file.");
+
+//Pre-compile all the email templates at startup, that way we know immediately if there's an error.
+_.each(Meteor.settings.MAIL.templates, function(settings, templateKey) {
+  if(settings.template)
+    SSR.compileTemplate(templateKey, Assets.getText('email_templates/{0}.html'.format(settings.template)));
+});
 
 Mailer = {
     sendTemplatedEmail: function(templateKey, overrides, context) {
@@ -13,30 +18,33 @@ Mailer = {
           settings: Meteor.settings
         }, context || {});
         
+        
         //You can override the templateSettings.template variable to render a different template in a different package.
         templateSettings = Meteor.settings.MAIL.templates[templateKey];
-        //This compiles templates as needed, and allows them to be overridden by child packages if they compile
-        //the template early enough in the lifecycle.
-        if(!Template[templateSettings.template]) {
-          //By default look up templates in the /private/email_templates folder
-          SSR.compileTemplate(templateKey, Assets.getText('email_templates/' + templateSettings.template + '.html'));
+        //If the template doesn't exist we shouldn't go any further.
+        if(!templateSettings)
+          throw new Meteor.Error(500, 'Tried to send an email with nonexistent template {0}.'.format(templateKey));
+          
+        try {        
+          /* 
+          * The settings for an email trickle down from most to least important:
+          * 1. Overrides passed in
+          * 2. Individual template settings
+          * 3. Default template settings
+          * 4. Settings in this file (like text)
+          */               
+          template = _.extend(
+              {
+                text: SSR.render(templateKey, context)
+              }, 
+              Meteor.settings.MAIL.templates.default, 
+              templateSettings,
+              overrides
+          );
+        }catch(e) {
+          //This means there was a general error either finding the template passed in or in rendering the compiled template.
+          throw new Meteor.Error(500, 'There was an error rendering the email template for {0} template.  {1}'.format(templateKey, e.toString()));
         }
-                
-        /* The settings for an email trickle down from most to least important:
-         * 1. Overrides passed in
-         * 2. Individual template settings
-         * 3. Default template settings
-         * 4. Settings in this file (like text)
-         */
-               
-        template = _.extend(
-            {
-              text: SSR.render(templateKey, context)
-            }, 
-            Meteor.settings.MAIL.templates.default, 
-            templateSettings,
-            overrides
-        );
         
         //Apply string formatting to every string variable in the template, this allows you to have a variable subject for example.
         template = _.object(_.map(template, function(value, key) {
@@ -52,7 +60,7 @@ Mailer = {
             Email.send(template);
         }catch(e) {
             //May not be the best error message, I'm not sure if including the error string could expose any sensitive information.
-            throw new Meteor.Error(500, 'There was a problem sending an email using the ' + templateKey + ' template.  ' + e.toString());
+            throw new Meteor.Error(500, 'There was a problem sending an email using the {0} template.  {1}.'.format(templateKey, e.toString()));
         }
     }
 }
